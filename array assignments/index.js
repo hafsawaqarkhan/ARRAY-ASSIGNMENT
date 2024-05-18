fbc444ac5c270f74520d3b4e0dc45f8d6ed24ac1
hafsawaqarkhan <163749368+hafsawaqarkhan@users.noreply.github.com>
2024-05-18 19:25:18 +0500

    Add files via upload

---
+++ array assignments/index.js
@@ -0,0 +1,29 @@
+// QUESTION NO 01
+var fruits = ["apple", "banana", "mango", "orange"];
+// QUESTION NO 02
+var numbers = [10, 20, 30, 40];
+// QUESTION NO 03
+console.log(fruits[2], ["third fruit"]);
+// QUESTION NO 04
+numbers[1] = 25;
+console.log(numbers);
+// QUESTION NO 05
+fruits.push('grapes');
+console.log(fruits);
+// QUESTION NO 06
+console.log(fruits.pop(), ["last fruit"]);
+// QUESTION NO 07
+console.log(fruits.shift(), ["first fruit"]);
+// QUESTION NO 08
+fruits.push('kiwi');
+// QUESTION NO 09
+fruits.shift();
+fruits.shift();
+// QUESTION N0 10
+fruits.unshift('pineapple');
+fruits.unshift('pear');
+// QUESTION NO 11
+var citrusFruit = ["pineaaple", "pear"];
+// QUESTION NO 12 
+var lastTwofruits = ["mango", "orange"];
+console.log(fruits);
---
+++ array assignments/index.ts
@@ -0,0 +1,52 @@
+// QUESTION NO 01
+
+const fruits = [ "apple" , "banana" , "mango" , "orange"]
+
+// QUESTION NO 02
+
+const numbers = [ 10 , 20 , 30 ,40]
+
+// QUESTION NO 03
+
+console.log(fruits[2], ["third fruit"]);
+
+// QUESTION NO 04
+
+numbers[1]=25
+console.log(numbers);
+
+// QUESTION NO 05
+
+fruits.push('grapes')
+console.log(fruits);
+
+// QUESTION NO 06
+
+console.log (fruits.pop() , ["last fruit"]);
+
+// QUESTION NO 07
+
+console.log(fruits.shift() , ["first fruit"]);
+
+// QUESTION NO 08
+
+fruits.push('kiwi')
+
+// QUESTION NO 09
+
+fruits.shift()
+fruits.shift()
+
+// QUESTION N0 10
+
+fruits.unshift('pineapple')
+fruits.unshift('pear')
+
+// QUESTION NO 11
+
+const citrusFruit = [ "pineaaple", "pear"]
+
+// QUESTION NO 12 
+
+const lastTwofruits = ["kiwi" , "orange"]
+ console.log(fruits)
